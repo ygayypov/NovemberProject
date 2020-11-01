32e889b8f735f4c53d5db8aa8d33321f1575734d
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-11-01 08:55:00 -0700

    Have a little commit in Batman

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,6 +3,8 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("Fantastic");
         System.out.println("I will catch you Joker");
+
+        //So close to catch but really have to find time
     }
 
     //The new commit from this class

7fe60754ebb6c7e70a422f29a0ba03bbc70fadbe
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-11-01 08:54:00 -0700

    Batman said I will catch Joker

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,6 +2,7 @@ public class Batman {
 
     public static void main(String[] args) {
         System.out.println("Fantastic");
+        System.out.println("I will catch you Joker");
     }
 
     //The new commit from this class

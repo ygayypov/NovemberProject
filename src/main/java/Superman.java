4c1c81171d96e02be340286a2fffc38e1d45880b
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-11-01 08:39:59 -0700

    Added print statement Superman

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -3,5 +3,6 @@ public class Superman {
     public static void main(String[] args) {
         System.out.println("New November Project on the way");
         System.out.println("I thing it is interesting work on the Intellij and GitHub");
+        System.out.println("Superman");
     }
 }

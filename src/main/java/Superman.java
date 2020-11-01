5f7f13450210348f0336ec94940c8b1a17cd1089
ygayypov <68933447+ygayypov@users.noreply.github.com>
2020-11-01 08:36:14 -0700

    Superman and Batman classes first commits

---
+++ src/main/java/Superman.java
@@ -0,0 +1,7 @@
+public class Superman {
+
+    public static void main(String[] args) {
+        System.out.println("New November Project on the way");
+        System.out.println("I thing it is interesting work on the Intellij and GitHub");
+    }
+}
---
+++ src/test/java/Batman.java
@@ -0,0 +1,9 @@
+public class Batman {
+
+    public static void main(String[] args) {
+        System.out.println("Fantastic");
+    }
+
+    //The new commit from this class
+
+}
